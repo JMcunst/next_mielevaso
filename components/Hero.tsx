135596d70e8429da6edfc774ae7235e179935c6a
jmcunst <xnslqjtmghaf@gmail.com>
2023-06-22 22:43:35 +0900

    feat: main page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,38 @@
+"use client";
 import React from 'react'
+import Image from 'next/image'
+import CustomButton from './CustomButton'
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+    const handleScroll = () => { }
+
+    return (
+        <div className='hero'>
+            <div className='flex-1 pt-36 padding-x'>
+                <h1 className='hero__title'>
+                    Welcome to Mielevaso App
+                </h1>
+
+                <p className='hero_subtitle'>
+                    미엘레바소 기사단입니다.
+                </p>
+
+                <CustomButton
+                    title="Explore Our Guild"
+                    containerStyles="bg-primary-blue
+                text-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
+            </div>
+            <div className='hero__image-container'>
+                <div className='hero__image'>
+                    <Image src="/mielevaso.png" rounded-full alt="hero"
+                     fill className="object-contain" />
+                     <div className='hero__image-overlay'></div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Hero
