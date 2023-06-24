cbf06ac70f420e930984227b5fc1ff8c41e8a370
jmcunst <xnslqjtmghaf@gmail.com>
2023-06-24 21:38:13 +0900

    update: car card image size

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -27,7 +27,7 @@ const CarCard = ({ car }: CarCardProps) => {
             </p>
 
             <div className='relative w-full h-40 my-3 object-contain'>
-                <Image src="/hero.png" width={50} height={50} alt="car model" fill priority className='object-contain'/>
+                <Image src="/hero.png" alt="car model" fill priority className='object-contain'/>
             </div>
         </div>
     )
