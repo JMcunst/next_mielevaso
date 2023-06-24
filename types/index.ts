07297de91f7c1c32bc025162039b928b8b96b097
jmcunst <xnslqjtmghaf@gmail.com>
2023-06-24 17:38:44 +0900

    feat: type of custom filter

--- types/index.ts
+++ types/index.ts
@@ -11,3 +11,13 @@ export interface SearchManufacturerProps {
     manufacturer: string;
     setManufacturer: (manufacturer: string) => void;
 }
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+}
