e39fe68cddc60594fcfe15ca8675bc30baa33bb6
jmcunst <xnslqjtmghaf@gmail.com>
2023-06-24 21:49:55 +0900

    update: custom button for car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 import { CarProps } from '@/types'
 import { calculateCarRent } from '@/utils';
+import CustomButton from './CustomButton';
 
 interface CarCardProps {
     car: CarProps;
@@ -11,6 +12,8 @@ interface CarCardProps {
 
 const CarCard = ({ car }: CarCardProps) => {
     const { city_mpg, year, make, model, transmission, drive } = car;
+
+    const [isOpen, setIsOpen] = useState(false);
 
     const carRent = calculateCarRent(city_mpg, year);
     return (
@@ -46,6 +49,16 @@ const CarCard = ({ car }: CarCardProps) => {
                         <Image src="/gas.svg" width={20} height={20} alt="seat" />
                         <p className="car-card__icon-text">{city_mpg} MPG</p>
                     </div>
+                </div>
+
+                <div className="car-card__btn-container">
+                    <CustomButton
+                        title='View More'
+                        containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+                        textStyles='text-white text-[14px] leading-[17px] font-bold'
+                        rightIcon='/right-arrow.svg'
+                        handleClick={() => setIsOpen(true)}
+                    />
                 </div>
 
             </div>
